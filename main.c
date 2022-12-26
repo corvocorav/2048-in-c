31a517649fde2452857f067bc40c541c751054f3
corvocorav <corvojaeger@gmail.com>
2022-12-26 13:48:29 +0100

    almost done

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include<string.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 
-int disp(); int loopRight(); int loopLeft(); int loopUp(); int loopDown(); int sweep(int direction);
-int tab[4][4] = {   {2,2,2,4},
-                    {2,0,0,2},
-                    {4,0,0,0},
-                    {4,2,0,2}
+int disp(); int loopRight(); int loopLeft(); int loopUp(); int loopDown(); int sweep(int direction); int clear(); //int spawn(int x , int y,int spot);
+int tab[4][4] = {   {2,0,2,0},
+                    {0,0,0,0},
+                    {0,0,0,0},
+                    {0,0,0,0}
               };
 int main()
 {
@@ -42,7 +43,7 @@ int main()
         loopDown();
         sweep(4);
     }
-    else  return 0; 
+    else if(!(strcmp(cmd , "out"))) return 0; 
     
     //wait
 
@@ -168,13 +169,27 @@ int loopDown()
 	
 }
 int disp()
-{   printf("\n\n");    
+{   printf("\n\n"); 
+    printf("\n-------------------------\n");
     for(int x = 0 ; x<4 ; x++)
-    {   printf("\n");
+    {   
         for(int y = 0; y<4; y++)
         {
-        printf("%d " , tab[x][y]);
+            //see number of spaces in it
+        if((tab[x][y]/10) == 0)
+        {
+            printf("|  %d  " , tab[x][y]);
         }
+        else if((tab[x][y]/100) == 0)
+        {
+            printf("| %d  " , tab[x][y]);
+        }
+        else if((tab[x][y]/1000) == 0)
+        {
+            printf("|%d " , tab[x][y]);
+        }
+        }
+        printf("|\n-------------------------\n");
     }
 }
 int sweep(int direction)
@@ -239,4 +254,59 @@ int sweep(int direction)
     }
     }
  //   
+ clear();
 }
+
+int clear()
+{   
+    int Xs[16]; int Ys[16];
+    
+
+    int emptySpots = 0;
+    int spawnCoords = 0;
+
+    for(int x = 0 ; x<4 ; x++)
+    {   
+        for(int y = 0; y<4; y++)
+        {
+
+        if(tab[x][y] == 0) 
+        {
+        //    printf("\n empty %d " , tab[x][y]);
+            
+            Xs[emptySpots] = x;
+            Ys[emptySpots] = y;
+            emptySpots += 1;
+      //      printf("\n empty at (%d,%d) number %d" , Xs[emptySpots],Ys[emptySpots],emptySpots);
+           // spawn(x,y,emptySpots);
+        }
+
+        }
+        
+    }
+    if(emptySpots > 0)
+    {
+    //    printf("\nthere is %d empty places" , emptySpots);
+        spawnCoords = (rand() % (emptySpots));
+        spawnCoords = (rand() % (emptySpots));
+        
+    //    printf("\nrand is %d" , spawnCoords);
+        //here spawn a num
+    //    printf("spawning at (%d,%d)" ,Xs[spawnCoords],Ys[spawnCoords]);
+        tab[Xs[spawnCoords]][Ys[spawnCoords]] = 2;
+    }
+    else printf("\n ####GAME OVER#####");
+}
+/*
+int spawn(int x , int y,int spot)
+{   
+
+    int Xs[16] ; int Ys[16];
+    Xs[spot] = x;
+    Ys[spot] = y;
+    
+    printf("\n empty at (%d,%d) number %d" , Xs[spot],Ys[spot],spot);
+  //  printf("\n empty at ( %d , %d ) number %d" , x,y, spot);
+    
+
+}*/
